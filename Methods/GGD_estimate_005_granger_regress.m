0bf10dbd7b7797d42bf2155e0023bff561bdaa77
yyzcqupt <129082268+yyzcqupt@users.noreply.github.com>
2024-06-02 01:43:25 +0800

    Add files via upload

---
+++ Methods/GGD_estimate_005_granger_regress.m
@@ -0,0 +1,18 @@
+function [ret] = GGD_estimate_005_granger_regress(Data,nlags)
+
+nvar = size(Data,1);
+[ Y,A ] = X_Y( Data,nlags );
+for ii=1:nvar
+    xdep = Y(:,ii);
+    X(:,ii) = GGD_estimate_005(xdep, A);
+    Y_hat(:,ii) = A*X(:,ii);
+end
+% 统计检验删除部分连接
+[gc,gc_by] = NetEstSig(X);  %前面为保留连接强度信息的连接，后面为二值化的连接
+ret.gc = gc;
+ret.gc_by = gc_by;
+ret.MVAR = X;
+ret.y_outlier = Y;
+ret.y_outlier_MVAR = Y_hat;
+end
+
---
+++ Methods/SBL_granger_regress.m
@@ -0,0 +1,18 @@
+function [ret] = SBL_granger_regress(Data,nlags)
+
+nvar = size(Data,1);
+[ Y,A ] = X_Y( Data,nlags );
+for ii=1:nvar
+    xdep = Y(:,ii);
+    X(:,ii) = SBL_estimate(xdep, A,0,1);
+    Y_hat(:,ii) = A*X(:,ii);
+end
+% 统计检验删除部分连接
+[gc,gc_by] = NetEstSig(X);  %前面为保留连接强度信息的连接，后面为二值化的连接
+ret.gc = gc;
+ret.gc_by = gc_by;
+ret.MVAR = X;
+ret.y_outlier = Y;
+ret.y_outlier_MVAR = Y_hat;
+end
+

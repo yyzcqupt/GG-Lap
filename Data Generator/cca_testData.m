1126548618f847e57f52d7825cdc12709c64a31d
yyzcqupt <129082268+yyzcqupt@users.noreply.github.com>
2024-06-02 01:42:17 +0800

    Add files via upload

---
+++ Data Generator/cca_testData.m
@@ -0,0 +1,221 @@
+function X = cca_testData(N,FLAG)
+%-------------------------------------------------------------------
+%   FUNCTION:   ccaTestData.m
+%   PURPOSE:    generate sample data for Granger analysis
+%
+%   INPUT:  N       -   # timepoints
+%           FLAG    -   select generative model (see below)
+%
+%   OUTPUT: X       -   nvar * nobs matrix of observations
+
+%   FLAG == 1:
+%   Baccala & Sameshima data
+%   Baccala, L. A. & Sameshima, K. 2001 Partial directed coherence:
+%   a new concept in neural structure determination. Biol Cybern 84,
+%   463-74.
+
+%   FLAG == 2:
+%   Schelter et al data
+%   Schelter, B., Winterhalder, M., Eichler, M., Peifer, M., Hellwig, B.,
+%   Guschlbauer, B., Lucking, C. H., Dahlhaus, R. & Timmer, J. 2006
+%   Testing for directed influences among neural signals using partial directed coherence.
+%   J Neurosci Methods 152, 210-9.
+
+%   FLAG == 3:
+%   Guo, S., Seth, A. K., Kendrick, K. M., Zhou, C., & Feng, J. (2008).
+%   Partial Granger causality-Eliminating exogenous inputs and latent variables.
+%   J Neurosci Methods, 172(1), 79-93.  (Figure 5C)
+
+%   FLAG == 4,
+%   4 variables, x2->x1 and x4->x3, no conditional relations,
+%   use to demonstrate bootstrap/permutation for spectral G-causality
+
+%   Written by AKS, Apr 02 2009
+% COPYRIGHT NOTICE AT BOTTOM
+%----------------------------------------------------------------
+
+settleTime = 500;   % number of observations to allow dynamics to settle
+
+if FLAG == 1  % Baccala & Sameshima
+
+    N = N + settleTime;
+    X = cca_normrnd(0,1,5,N);   % 5 variables
+    r = sqrt(2);
+    for i=4:N
+        X(1,i) = X(1,i) + 0.95.*r.*X(1,i-1) - 0.9025.*X(1,i-2);
+        X(2,i) = X(2,i) + 0.5.*X(1,i-2) ;
+        X(3,i) = X(3,i) - 0.4.*X(1,i-3);
+        X(4,i) = X(4,i) - 0.5.*X(1,i-2) + 0.25.*r.*X(4,i-1) + 0.25.*r.*X(5,i-1);
+        X(5,i) = X(5,i) - 0.25.*r.*X(4,i-1) + 0.25.*r.*X(5,i-1);
+
+    end
+    X = X(:,settleTime+1:end);
+
+elseif FLAG == 2   % Schelter et al
+
+    N = N + settleTime;
+    X = cca_normrnd(0,1,5,N);   % 5 variables
+    for i=5:N
+        X(1,i) = X(1,i) + 0.6.*X(1,i-1) + 0.65.*X(2,i-2);
+        X(2,i) = X(2,i) + 0.5.*X(2,i-1) - 0.3.*X(2,i-2) - 0.3.*X(3,i-4) + 0.6.*X(4,i-1);
+        X(3,i) = X(3,i) + 0.8.*X(3,i-1) - 0.7.*X(3,i-2) - 0.1.*X(5,i-3);
+        X(4,i) = X(4,i) + 0.5.*X(4,i-1) + 0.9.*X(3,i-2) + 0.4.*X(5,i-2);
+        X(5,i) = X(5,i) + 0.7.*X(5,i-1) - 0.5.*X(5,i-2) - 0.2.*X(3,i-1);
+    end
+    X = X(:,settleTime+1:end);
+
+
+elseif FLAG == 3   % Guo et al, Fig 5C
+
+    N = N + settleTime;
+    X = zeros(5,N);
+    X(:,1:3) = cca_normrnd(0,1,5,3);
+
+    % set noise and error terms
+    v = [0.8 0.6 1.0 1.2 1.0 0.9 1.0]; % variances for error
+    W = zeros(7,N);
+    for i=1:7
+        W(i,:) = normrnd(0,sqrt(v(i)),1,N);
+    end
+    a = rand(1,5);
+    b = rand(1,5);
+    c = [1 1 1 1 1];
+    r = sqrt(2);
+
+    for i=4:N
+        X(1,i) = 0.95.*r.*X(1,i-1) - 0.9025.*X(1,i-2) + W(1,i) + a(1).*W(6,i) + b(1).*W(7,i-1) + c(1).*W(7,i-2);
+        X(2,i) = 0.5.*X(1,i-2) + W(2,i) + a(2).*W(6,i) + b(2).*W(7,i-1) + c(2).*W(7,i-2);
+        X(3,i) = 0.4.*X(1,i-3).*-1 + W(3,i) + a(3).*W(6,i) + b(3).*W(7,i-1) + c(3).*W(7,i-2);
+        X(4,i) = 0.5.*X(1,i-2).*-1 + 0.25.*r.*X(4,i-1) + 0.25.*r.*X(5,i-1) + W(4,i) + a(4).*W(6,i) + ...
+            b(4).*W(7,i-1) + c(4).*W(7,i-2);
+        X(5,i) = 0.25.*r.*X(4,i-1).*-1 + 0.25.*r.*X(5,i-1) ...
+            + W(5,i) + a(5).*W(6,i) + b(5).*W(7,i-1) + c(5).*W(7,i-2);
+    end
+    X = X(:,settleTime+1:end);
+    
+elseif FLAG == 4
+    
+    N = N + settleTime;
+    X = cca_normrnd(0,1,4,N);   % 4 variables
+    r = sqrt(2);
+    for i=4:N
+        X(1,i) = X(1,i) + 0.95.*r.*X(1,i-1) - 0.9025.*X(1,i-2);
+        X(2,i) = X(2,i) + 0.5.*X(1,i-1);
+        X(3,i) = X(3,i) - 0.4.*X(4,i-3);
+        X(4,i) = X(4,i) + 0.35.*X(4,i-2);
+    end
+    X = X(:,settleTime+1:end);
+    
+elseif FLAG == 6   %SBL—LAP论文仿真
+    
+    N = N + settleTime;
+    X = cca_normrnd(0,1,6,N);   % 6 variables 
+    for i=4:N
+        X(1,i) = X(1,i) + 0.22.*X(1,i-1) + 0.56.*X(4,i-2);
+        X(2,i) = X(2,i) - 0.22.*X(2,i-1) + 0.55.*X(1,i-1);
+        X(3,i) = X(3,i) + 0.48.*X(2,i-3);
+        X(4,i) = X(4,i) + 0.51.*X(1,i-2) + 0.85.*X(3,i-3);
+        X(5,i) = X(5,i) + 0.42.*X(4,i-1) + 0.40.*X(6,i-2);
+        X(6,i) = X(6,i) + 0.65.*X(1,i-2);
+    end
+    X = X(:,settleTime+1:end);
+    
+elseif FLAG == 7
+    
+    N = N + settleTime;
+    X = cca_normrnd(0,1,5,N);   % 5 variables 
+    for i=4:N
+        X(1,i) = X(1,i) + 0.95.*sqrt(2).*X(1,i-1) - 0.9025.*X(1,i-2);
+        X(2,i) = X(2,i) + 0.5.*X(1,i-2) - 0.4.*X(3,i-2);
+        X(3,i) = X(3,i) - 0.4.*X(1,i-3) + 0.4.*X(2,i-2);
+        X(4,i) = X(4,i) - 0.5.*X(1,i-2) + 0.25.*sqrt(2).*X(4,i-1) + 0.25.*sqrt(2).*X(5,i-1);
+        X(5,i) = X(5,i) - 0.25.*sqrt(2).*X(4,i-1) + 0.25.*sqrt(2).*X(5,i-1);
+    end
+    X = X(:,settleTime+1:end);
+
+elseif FLAG == 8
+    
+    N = N + settleTime;
+    X = cca_normrnd(0,1,8,N);   % 8 variables
+    for i=7:N
+        X(1,i) = X(1,i) + 0.4.*X(8,i-1) - 0.55.*X(8,i-2) + 0.3.*X(1,i-1) + 0.4.*X(8,i-3);
+        X(2,i) = X(2,i) + 0.5.*X(1,i-1) + 0.6.*X(2,i-1) + 0.4.*X(2,i-2);
+        X(3,i) = X(3,i) - 0.4.*X(4,i-3) + 0.7.*X(4,i-2) + 0.5.*X(4,i-1) - 0.25.*X(3,i-3) + 0.25.*X(3,i-1);
+        X(4,i) = X(4,i) - 0.5.*X(5,i-2) + 0.25.*X(8,i-2) + 0.32.*X(8,i-1) + 0.25.*X(5,i-1) + 0.5.*X(8,i-3);
+        X(5,i) = X(5,i) - 0.25.*X(5,i-2) + 0.25.*X(6,i-1) - 0.5.*X(6,i-2) + 0.25.*X(5,i-1);
+        X(6,i) = X(6,i) + 0.25.*X(5,i-2) + 0.25.*X(7,i-2) + 0.41.*X(7,i-1) - 0.25.*X(5,i-1) + 0.25.*X(7,i-3);
+        X(7,i) = X(7,i) - 0.5.*X(8,i-3) + 0.25.*X(8,i-2) + 0.31.*X(8,i-1) - 0.5.*X(7,i-2) + 0.2.*X(7,i-1);
+        X(8,i) = X(8,i) - 0.25.*X(8,i-6) + 0.25.*X(8,i-4) + 0.33.*X(8,i-3) + 0.25.*X(8,i-2) + 0.25.*X(8,i-1);
+%   
+    end
+    X = X(:,settleTime+1:end);
+    
+
+elseif FLAG == 9    %自己创立的仿真模型
+    
+    N = N + settleTime;
+    X = cca_normrnd(0,1,6,N);   % 6 variables 
+    for i=4:N
+        X(1,i) = X(1,i) + 0.30.*X(1,i-1) + 0.67.*X(4,i-2);
+        X(2,i) = X(2,i) - 0.50.*X(2,i-1) + 0.80.*X(1,i-1) + 0.70.*X(3,i-3);
+        X(3,i) = X(3,i) + 0.45.*X(2,i-3) + 0.31.*X(1,i-2);
+        X(4,i) = X(4,i) + 0.71.*X(1,i-2) + 0.57.*X(3,i-3);
+        X(5,i) = X(5,i) + 0.83.*X(4,i-1) + 0.61.*X(6,i-2) + 0.73.*X(1,i-3);
+        X(6,i) = X(6,i) + 0.49.*X(1,i-2);
+%   
+    end
+    X = X(:,settleTime+1:end);
+
+
+elseif FLAG == 10    %自己创立的仿真模型（2）
+    
+    N = N + settleTime;
+    X = cca_normrnd(0,1,6,N);   % 6 variables 
+    for i=4:N
+        X(1,i) = X(1,i) + 0.30.*X(1,i-1) + 0.67.*X(5,i-2);
+        X(2,i) = X(2,i) - 0.50.*X(2,i-1) + 0.80.*X(1,i-1) + 0.70.*X(3,i-3);
+        X(3,i) = X(3,i) + 0.45.*X(2,i-3);
+        X(4,i) = X(4,i) + 0.57.*X(3,i-3);
+        X(5,i) = X(5,i) + 0.83.*X(4,i-1) + 0.61.*X(6,i-2);
+        X(6,i) = X(6,i) + 0.49.*X(1,i-2);
+%   
+    end
+    X = X(:,settleTime+1:end);
+
+
+
+
+elseif FLAG == 11    %自己创立的仿真模型（3）
+    
+    N = N + settleTime;
+    X = cca_normrnd(0,1,6,N);   % 6 variables 
+    for i=4:N
+        X(1,i) = X(1,i) + 0.30.*X(1,i-1) + 0.67.*X(5,i-2);
+        X(2,i) = X(2,i) - 0.50.*X(2,i-1) + 0.80.*X(1,i-1) + 0.70.*X(3,i-3);
+        X(3,i) = X(3,i) + 0.45.*X(2,i-3);
+        X(4,i) = X(4,i) + 0.57.*X(3,i-3) + 0.79.*X(2,i-1);
+        X(5,i) = X(5,i) + 0.83.*X(4,i-1) + 0.61.*X(6,i-2);
+        X(6,i) = X(6,i) + 0.49.*X(1,i-2);
+%   
+    end
+    X = X(:,settleTime+1:end);
+else
+    error('invalid input to ccaTestData.m');
+end
+
+
+% This file is part of GCCAtoolbox.  
+% It is Copyright (C) Anil Seth (2005-2009) 
+% 
+% GCCAtoolbox is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% GCCAtoolbox is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with GCCAtoolbox. If not, see <http://www.gnu.org/licenses/>.
